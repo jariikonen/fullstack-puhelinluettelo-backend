8f87d024c0cca4eecc9a56cfeb43686cae82bd7f
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-12-09 14:43:40 +0200

    teht. 3.3

--- index.js
+++ index.js
@@ -42,8 +42,20 @@ app.get('/info', (request, response) => {
     )
 })
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
