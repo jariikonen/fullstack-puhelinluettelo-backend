7c3e3121b321c2215adf2983ea8489c50028e8f7
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2022-12-11 10:42:49 +0200

    teht. 3.8

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const morgan = require('morgan')
 
 const app = express()
 
+morgan.token('data', (request, response) => {
+    if (Object.values(request.body).length > 0) {
+        return JSON.stringify(request.body)
+    }
+    return ' '
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :data'
+))
 
 let persons = [
     { 
