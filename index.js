af84b65f1c83352e3ba5e94ec570a5eb59ffb00b
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-01-06 07:55:07 +0200

    teht. 3.16

--- index.js
+++ index.js
@@ -66,8 +66,7 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
+  const person = persons.find(p => p.id === Number(request.params.id))
 
   if (person) {
     response.json(person)
@@ -77,35 +76,27 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
-    .catch(error => {
-      console.error(error)
-      if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-      }
-      else {
-        response.status(500).end()
-      }
-    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name) {
-    return response.status(400).json({error: 'name missing'})
+    return response.status(400).json({ error: 'name missing' })
   }
   if (!body.number) {
-    return response.status(400).json({error: 'number missing'})
+    return response.status(400).json({ error: 'number missing' })
   }
 
   const found = persons.find(person => person.name === body.name)
   if (found) {
-    return response.status(400).json({error: 'name must be unique'})
+    return response.status(400).json({ error: 'name must be unique' })
   }
 
   const person = new Person({
@@ -117,6 +108,26 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
